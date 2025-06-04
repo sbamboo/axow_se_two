a18ddeb50b66a5ea6fd8f818e0d26637806c8979
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-06-05 01:04:33 +0200

    add perm

--- backend/site/_db_/presets.sql
+++ backend/site/_db_/presets.sql
@@ -35,55 +35,60 @@ INSERT INTO user_permissions (string, digit_index, digit) VALUES
     ("articles-subcat.add-remove", 3, 6),
     ("articles-subcat.remove-modify", 3, 7);
 
--- Index 4 (wiki)
+-- Index 4 (article-own)
 INSERT INTO user_permissions (string, digit_index, digit) VALUES 
-    ("wiki.*", 4, 1),
-    ("wiki.add", 4, 2),
-    ("wiki.modify", 4, 3),
-    ("wiki.remove", 4, 4),
-    ("wiki.add-modify", 4, 5),
-    ("wiki.add-remove", 4, 6),
-    ("wiki.remove-modify", 4, 7);
+    ("all-articles", 4, 1),
+    ("your-articles", 4, 2);
 
--- Index 5 (wiki-page)
+-- Index 5 (wiki)
 INSERT INTO user_permissions (string, digit_index, digit) VALUES 
-    ("wiki-page.*", 5, 1),
-    ("wiki-page.add", 5, 2),
-    ("wiki-page.modify", 5, 3),
-    ("wiki-page.remove", 5, 4),
-    ("wiki-page.add-modify", 5, 5),
-    ("wiki-page.add-remove", 5, 6),
-    ("wiki-page.remove-modify", 5, 7);
+    ("wiki.*", 5, 1),
+    ("wiki.add", 5, 2),
+    ("wiki.modify", 5, 3),
+    ("wiki.remove", 5, 4),
+    ("wiki.add-modify", 5, 5),
+    ("wiki.add-remove", 5, 6),
+    ("wiki.remove-modify", 5, 7);
 
--- Index 6 (wiki-cat)
+-- Index 6 (wiki-page)
 INSERT INTO user_permissions (string, digit_index, digit) VALUES 
-    ("wiki-cat.*", 6, 1),
-    ("wiki-cat.add", 6, 2),
-    ("wiki-cat.modify", 6, 3),
-    ("wiki-cat.remove", 6, 4),
-    ("wiki-cat.add-modify", 6, 5),
-    ("wiki-cat.add-remove", 6, 6),
-    ("wiki-cat.remove-modify", 6, 7);
+    ("wiki-page.*", 6, 1),
+    ("wiki-page.add", 6, 2),
+    ("wiki-page.modify", 6, 3),
+    ("wiki-page.remove", 6, 4),
+    ("wiki-page.add-modify", 6, 5),
+    ("wiki-page.add-remove", 6, 6),
+    ("wiki-page.remove-modify", 6, 7);
 
--- Index 7 (profiles)
+-- Index 7 (wiki-cat)
 INSERT INTO user_permissions (string, digit_index, digit) VALUES 
-    ("profiles.*", 7, 1),
-    ("profiles.add", 7, 2),
-    ("profiles.modify", 7, 3),
-    ("profiles.remove", 7, 4),
-    ("profiles.add-modify", 7, 5),
-    ("profiles.add-remove", 7, 6),
-    ("profiles.remove-modify", 7, 7);
+    ("wiki-cat.*", 7, 1),
+    ("wiki-cat.add", 7, 2),
+    ("wiki-cat.modify", 7, 3),
+    ("wiki-cat.remove", 7, 4),
+    ("wiki-cat.add-modify", 7, 5),
+    ("wiki-cat.add-remove", 7, 6),
+    ("wiki-cat.remove-modify", 7, 7);
 
--- Index 8 (profile scope)
+-- Index 8 (profiles)
 INSERT INTO user_permissions (string, digit_index, digit) VALUES 
-    ("all-profiles", 8, 1),
-    ("your-profile", 8, 2);
+    ("profiles.*", 8, 1),
+    ("profiles.add", 8, 2),
+    ("profiles.modify", 8, 3),
+    ("profiles.remove", 8, 4),
+    ("profiles.add-modify", 8, 5),
+    ("profiles.add-remove", 8, 6),
+    ("profiles.remove-modify", 8, 7);
 
--- Index 9 (url-preview)
+-- Index 9 (profile scope)
 INSERT INTO user_permissions (string, digit_index, digit) VALUES 
-    ("url-preview.*", 9, 1),
-    ("url-preview.fetch", 9, 2);
+    ("all-profiles", 9, 1),
+    ("your-profile", 9, 2);
+
+-- Index 10 (url-preview)
+INSERT INTO user_permissions (string, digit_index, digit) VALUES 
+    ("url-preview.*", 10, 1),
+    ("url-preview.fetch", 10, 2);
 
 -- Add the admin user with full permissions
 INSERT INTO users (username, password_hash) VALUES ("admin", "$2y$10$jYCWrLmfdm9MGrvKJ5D5yOwS3a0Bi6W5u1w0AXc9.0rIzCkZb9coi"); -- Temp "admin" password (sha256) CHANGE TO SAFER IN PROD
--- docs/auth.md
+++ docs/auth.md
@@ -42,9 +42,10 @@
 |  `1`  | `0-7`       | `0`: None; `1`: `articles.*`; `2`: `articles.add`; `3`: `articles.modify`; `4`: `articles.remove`; `5`: `articles.add-modify`; `6`: `articles.add-remove`; `7`: `articles.remove-modify` |
 |  `2`  | `0-7`       | `0`: None; `1`: `articles-cat.*`; `2`: `articles-cat.add`; `3`: `articles-cat.modify`; `4`: `articles-cat.remove`; `5`: `articles-cat.add-modify`; `6`: `articles-cat.add-remove`; `7`: `articles-cat.remove-modify` |
 |  `3`  | `0-7`       | `0`: None; `1`: `articles-subcat.*`; `2`: `articles-subcat.add`; `3`: `articles-subcat.modify`; `4`: `articles-subcat.remove`; `5`: `articles-subcat.add-modify`; `6`: `articles-subcat.add-remove`; `7`: `articles-subcat.remove-modify` |
-|  `4`  | `0-7`       | `0`: None; `1`: `wiki.*`; `2`: `wiki.add`; `3`: `wiki.modify`; `4`: `wiki.remove`; `5`: `wiki.add-modify`; `6`: `wiki.add-remove`; `7`: `wiki.remove-modify` |
-|  `5`  | `0-7`       | `0`: None; `1`: `wiki-page.*`; `2`: `wiki-page.add`; `3`: `wiki-page.modify`; `4`: `wiki-page.remove`; `5`: `wiki-page.add-modify`; `6`: `wiki-page.add-remove`; `7`: `wiki-page.remove-modify` |
-|  `6`  | `0-7`       | `0`: None; `1`: `wiki-cat.*`; `2`: `wiki-cat.add`; `3`: `wiki-cat.modify`; `4`: `wiki-cat.remove`; `5`: `wiki-cat.add-modify`; `6`: `wiki-cat.add-remove`; `7`: `wiki-cat.remove-modify` |
-|  `7`  | `0-7`       | `0`: None; `1`: `profiles.*`; `2`: `profiles.add`; `3`: `profiles.modify`; `4`: `profiles.remove`; `5`: `profiles.add-modify`; `6`: `profiles.add-remove`; `7`: `profiles.remove-modify` |
-|  `8`  | `0-2`       | `0`: None; `1`: `all-profiles`; `2`: `your-profile` |
-|  `9` | `0-2`       | `0`: None; `1`: `url-preview.*`; `2`: `url-preview.fetch` |
+|  `4`  | `0-2`       | `0`: None, `1`: `all-articles`; `2`: `your-articles` |
+|  `5`  | `0-7`       | `0`: None; `1`: `wiki.*`; `2`: `wiki.add`; `3`: `wiki.modify`; `4`: `wiki.remove`; `5`: `wiki.add-modify`; `6`: `wiki.add-remove`; `7`: `wiki.remove-modify` |
+|  `6`  | `0-7`       | `0`: None; `1`: `wiki-page.*`; `2`: `wiki-page.add`; `3`: `wiki-page.modify`; `4`: `wiki-page.remove`; `5`: `wiki-page.add-modify`; `6`: `wiki-page.add-remove`; `7`: `wiki-page.remove-modify` |
+|  `7`  | `0-7`       | `0`: None; `1`: `wiki-cat.*`; `2`: `wiki-cat.add`; `3`: `wiki-cat.modify`; `4`: `wiki-cat.remove`; `5`: `wiki-cat.add-modify`; `6`: `wiki-cat.add-remove`; `7`: `wiki-cat.remove-modify` |
+|  `8`  | `0-7`       | `0`: None; `1`: `profiles.*`; `2`: `profiles.add`; `3`: `profiles.modify`; `4`: `profiles.remove`; `5`: `profiles.add-modify`; `6`: `profiles.add-remove`; `7`: `profiles.remove-modify` |
+|  `9`  | `0-2`       | `0`: None; `1`: `all-profiles`; `2`: `your-profile` |
+|  `10` | `0-2`       | `0`: None; `1`: `url-preview.*`; `2`: `url-preview.fetch` |
